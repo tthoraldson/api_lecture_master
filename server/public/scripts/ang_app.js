102f834af3c927ca00dffc240d1a3ec2e078fe51
Kris Szafranski <kdszafranski@gmail.com>
2015-11-17 13:59:18 -0600

    complete with data response

--- server/public/scripts/ang_app.js
+++ server/public/scripts/ang_app.js
@@ -4,7 +4,17 @@ myApp.controller('APIController', ['$scope', '$http', function($scope, $http) {
     var apikey = "ca5d63fdb921e2570069eb8267389c4a9260227a";
     var baseUrl = "http://www.giantbomb.com/api";
 
-    // construct the URI with our apikey
+    // construct our URL
+    var gameSearchURL = baseUrl + '/search/?api_key=' + apikey + '&format=jsonp&limit=50';
+    var query = 'Batman & Robin';
+
+    var finalURL = gameSearchURL + '&query=' + encodeURI(query) + '&json_callback=JSON_CALLBACK';
+
+    $http.jsonp(finalURL).then(
+        function(response) {
+            console.log(response.data);
+        }
+    );
 
 
 }]);
--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -6,4 +6,26 @@ function gameSearch() {
     var apikey = "ca5d63fdb921e2570069eb8267389c4a9260227a";
     var baseUrl = "http://www.giantbomb.com/api";
 
+    // construct our URL
+    var gameSearchURL = baseUrl + '/search/?api_key=' + apikey + '&format=jsonp&limit=50';
+    var query = 'Batman & Robin';
+
+    var q = gameSearchURL + '&query=' + encodeURI(query);
+    console.log(q);
+
+    $.ajax({
+        type: "GET",
+        url: gameSearchURL + '&query=' + encodeURI(query),
+        dataType: "jsonp",
+        crossDomain: true,
+        jsonp: 'json_callback',
+        success: function(data) {
+            console.log(data);
+        }
+    });
+
+}
+
+function searchCallback(data) {
+    console.log(data);
 }
