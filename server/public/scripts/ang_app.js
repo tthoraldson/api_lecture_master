a6a5f688bc0dd41423522db19d0f2e6a99c03807
Kris Szafranski <kdszafranski@gmail.com>
2015-11-17 13:08:11 -0600

    reverted

--- server/public/scripts/ang_app.js
+++ server/public/scripts/ang_app.js
@@ -5,19 +5,6 @@ myApp.controller('APIController', ['$scope', '$http', function($scope, $http) {
     var baseUrl = "http://www.giantbomb.com/api";
 
     // construct the URI with our apikey
-    var GamesSearchUrl = baseUrl + '/search/?api_key=' + apikey + '&format=jsonp&limit=20';
-    var query = 'Batman';
-    var finalUrl = GamesSearchUrl + '&query=' + encodeURI(query) + '&json_callback=JSON_CALLBACK';
 
-    //finalUrl = 'http://numbersapi.com/42';
-    console.log('query: ', finalUrl);
-
-    // AJAX all the things!
-    $http.jsonp(finalUrl).then(
-        function(response) {
-            console.log(response.data.results);
-            $scope.data = response.data.results;
-        }
-    );
 
 }]);
