01005551367ec71d3e453d27a85c6e840540272c
Kris Szafranski <kdszafranski@gmail.com>
2016-05-25 13:13:25 -0500

    updated for mu lecture

--- server/app.js
+++ server/app.js
@@ -2,19 +2,19 @@ var express = require("express");
 var app = express();
 var path = require('path');
 
-app.set("port", process.env.PORT || 5000);
+// Serve back static files
+app.use(express.static(path.join(__dirname, './public')));
 
 app.get("/jq", function(req,res,next){
     res.sendFile(path.join(__dirname, "./public/views/indexjq.html"));
 });
 
-app.get("/*", function(req,res,next){
-    var file = req.params[0] || "views/index.html";
-    res.sendFile(path.join(__dirname, "./public/", file))
-});
+// Handle index file separately
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, './public/views/index.html'));
+})
 
-app.listen(app.get("port"), function(req,res,next){
-    console.log("Listening on port: " + app.get("port"));
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), function() {
+    console.log('Listening on port: ', app.get('port'));
 });
-
-module.exports = app;
--- server/public/scripts/ang_app.js
+++ server/public/scripts/ang_app.js
@@ -1,29 +1,44 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('APIController', ['$scope', '$http', function($scope, $http) {
-    $scope.data = {};
+    $scope.breed = '';
+    // API key
+    var key = 'b900e0d5e332753a460a64eaa8de00fd';
+    var baseURL = 'http://api.petfinder.com/';
 
-    function petFinder() {
-        // API key
-        var key = 'your api key here';
+    $scope.getRandomPet = function() {
+      var query = 'pet.getRandom';
+      query += '?key=' + key;
+      query += '&animal=dog';
+      query += '&output=basic';
+      query += '&format=json';
 
-        var baseURL = 'http://api.petfinder.com/';
-        var query = 'pet.getRandom';
-        query += '?key=' + key;
-        query += '&animal=dog';
-        query += '&output=basic';
-        query += '&format=json';
+      var request = baseURL + encodeURI(query) + '&callback=JSON_CALLBACK';
+      console.log(request);
 
-        var request = baseURL + encodeURI(query) + '&callback=JSON_CALLBACK';
-        console.log(request);
-
-        $http.jsonp(request).then(
-            function(response) {
-                $scope.animal = response.data.petfinder.pet;
-                console.log($scope.animal);
-            }
-        );
+      $http.jsonp(request).then(
+          function(response) {
+              $scope.animal = response.data.petfinder.pet;
+              console.log('random pet: ', $scope.animal);
+          }
+      );
     }
 
-    petFinder();
+    $scope.getBreeds = function() {
+      var query = 'breed.list';
+      query += '?key=' + key;
+      query += '&animal=' + $scope.breed.toLowerCase();
+      query += '&format=json';
+
+      var request = baseURL + encodeURI(query) + '&callback=JSON_CALLBACK';
+      console.log(request);
+
+      $http.jsonp(request).then(
+          function(response) {
+              console.log('breeds: ', response.data);
+              $scope.breeds = response.data.petfinder.breeds.breed;
+          }
+      );
+    }
+
 }]);
